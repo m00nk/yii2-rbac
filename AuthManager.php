71598c7f2f20f5902769cc7641a3b3b2e777c6f5
Dmitrij Sheremetjev <m00nk1975@gmail.com>
2015-08-01 03:31:06 +0300

    changed roles checking

--- AuthManager.php
+++ AuthManager.php
@@ -12,6 +12,26 @@ namespace m00nk\rbac;
 use Yii;
 use yii\base\Component;
 
+/*
+ * Особенность работы компонента в том, что для РОЛЕЙ условия анализируюится только если юзер имеет указанную роль, если же юзер не имеет указанной роли,
+ * то условие игнорируется. Это сделано для логичной работы в случае наследования ролей. Пример:
+ * - есть роль FREE_USER, которая имеет некое право 'permission' и условие, проверяющее, является ли данный пользователь FREE
+ * - есть роль PRO_USER, которая наследует в числе прочего роль FREE_USER. Естественно она имеет условие, проверяющее, является ли данный пользователь PRO
+ *
+ * Если бы для ролей анализировалось каждое условие в иерархии, то проверка для PRO прекращалась бы на уровне FREE_USER (т.к. PRO для системы не является FREE)
+ * и не доходила бы до PRO. Так работал алгоритм в версии до 1.2.1 включительно - для правильной обработки иерархии ролей необходимо было в проверке роли
+ * пользователя анализировать иерархию, чтобы эта проверка возвращала для PRO, что PRO == FREE, а для FREE, что FREE != PRO.
+ *
+ * Теперь эта проверка не нужна, т.к. иерархия ролей задается прамо в файле правил (что логичнее и проще). Для данной версии проверка роли пользователя должна
+ * быть реализована предельно просто:
+ *
+ * function hasRole($userId, $role)
+ * {
+ *      $user = self::loadUser($userId);
+ *      return $user ? $user->role == $role : $role == self::ROLE_GUEST;
+ * }
+ */
+
 class AuthManager extends Component
 {
 	public $authFile = '@app/config/auth.php';
@@ -76,10 +96,12 @@ class AuthManager extends Component
 
 	private function _checkItem(AuthItem $item, $params)
 	{
-		if(!$item->rule || eval($item->rule))
-		{
-			if($item->type == AuthItem::TYPE_ROLE) return true; // это роль - достигли цели
+		$rulePassed = !$item->rule || eval($item->rule);
 
+		if($item->type == AuthItem::TYPE_ROLE && $rulePassed) return true; // это роль - достигли цели
+
+		if($rulePassed || $item->type == AuthItem::TYPE_ROLE)
+		{
 			foreach($item->parents as $parent)
 				if($this->_checkItem($parent, $params) == true) return true;
 		}
